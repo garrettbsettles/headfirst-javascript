281f05d4dbd44e02f3ae5b1f91d1f9a252411eca
garrettbsettles <garrettbsettles@gmail.com>
2020-06-24 20:59:46 -0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-unused-vars
+const app: Element = document.querySelector('#root')!;
+
+// interface Car {
+// 	make: string;
+// 	model: string;
+// 	year: number;
+// 	color: string;
+// 	passenger: number,
+// }
+
+// const cadi = {
+// 	make: "GM",
+// 	model: "Cadillac",
+// 	year: 1955,
+// 	color: "tan",
+// 	passenger
+// }
